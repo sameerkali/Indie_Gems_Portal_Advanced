3479db6dd3e9f08f48c4ad5716dd30035d653bcf
Sameer Faridi <s.faridi007@gmail.com>
2024-01-20 22:09:50 +0530

    setup React Query in project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+const queryClient = new QueryClient();
+/*{
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      // cacheTime: 300_000,
+      staleTime: 10 * 1000,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false
+    }
+  }
+}*/
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
